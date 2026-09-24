f084010e163c5ad9b48e84d7b7001d000bc72e73
ev2e <ev2e@MacBook-Air-ev2e.local>
2026-09-24 05:49:45 -0700

    add new functions, fix some old functions

--- lab_1_classify_profile/main.py
+++ lab_1_classify_profile/main.py
@@ -6,6 +6,7 @@ Language detection
 
 # pylint:disable=unused-argument
 from typing import Sequence
+import string
 
 FreqDictType = dict[str, float]
 "Frequency dictionary. Contains pairs of token and its frequency."
@@ -17,10 +18,10 @@ ProfileType = tuple[str, FreqDictType, int]
 def tokenize(text: str) -> Sequence[str] | None:
     if not isinstance(text, str):
         return None
-    punctuation = '?!.,/;:*()^-_%<>&@#'
+    punctuation = string.punctuation
     text = text.lower()
     for sym in punctuation:
-        text = text.replace(sym,'')
+        text = text.replace(sym, '')
     return [token for token in text.split() if token]
 
     """
@@ -127,6 +128,26 @@ def create_language_profile(
         Returns None in case of incorrect input types.
     """
 
+    tokens = tokenize(text)
+    cl_tokens = remove_stop_words(tokens, stop_words)
+    freq_dict = calculate_frequencies(cl_tokens)
+
+    if not isinstance(stop_words, list) or not isinstance(text, str) or not isinstance(language,str):
+        return None
+
+    for word in stop_words:
+        if not isinstance(word, str):
+            return None
+
+    if freq_dict is None:
+        return None
+
+    if cl_tokens is None:
+        return None
+
+    language_profile = (language, freq_dict, len(freq_dict))
+    return language_profile
+
 
 def check_profile(profile: ProfileType) -> bool:
     """
@@ -139,11 +160,32 @@ def check_profile(profile: ProfileType) -> bool:
         bool: Returns True if the profile has right structure and types,
         otherwise returns False.
     """
+    if not isinstance(profile, tuple) or len(profile) != 3:
+        return False
+    if not isinstance(profile, tuple) or len(profile) != 3:
+        return False
+
+
+    if not isinstance(profile[0], str):
+        return False
+    if not isinstance(profile[1], dict):
+        return False
+    if not isinstance(profile[2], int) :
+        return False
+    for key, value in profile[1].items():
+        if not isinstance(key, str):
+            return False
+        if not isinstance(value, float):
+            return False
+
+        return True
+
 
 
 def compare_profiles_by_top_n(
     unknown_profile: ProfileType, profile_to_compare: ProfileType, top_n: int
 ) -> float | None:
+
     """
     Compares profiles and calculates the distance using top n words
 
@@ -155,6 +197,23 @@ def compare_profiles_by_top_n(
         float | None: The distance between profiles.
         Returns None in case of incorrect input types.
     """
+    if not check_profile(unknown_profile)  or not check_profile(profile_to_compare) or not isinstance(top_n,int) :
+        return None
+    unknown_profile_top_n = get_top_n_words(unknown_profile[1], top_n)
+    if unknown_profile_top_n is None:
+        return None
+
+    compare_profile_top_n = get_top_n_words(profile_to_compare[1], top_n)
+    if compare_profile_top_n is None:
+        return None
+
+    intersect = 0
+    for word in unknown_profile_top_n:
+        if word in compare_profile_top_n :
+            intersect += 1
+    intersection = intersect/ top_n
+
+    return intersection
 
 
 def detect_language_by_top_n(
@@ -173,6 +232,20 @@ def detect_language_by_top_n(
         str | None: Unknown profile language.
         Returns None in case of incorrect input types.
     """
+    if not check_profile(unknown_profile)  or not check_profile(profile_1) or not check_profile(profile_2) or not isinstance(top_n,int):
+            return None
+    sim1 = compare_profiles_by_top_n(unknown_profile, profile_1, top_n)
+    sim2 = compare_profiles_by_top_n(unknown_profile, profile_2, top_n)
+    if sim1 is None or sim2 is None:
+        return None
+    if sim1>sim2:
+        return profile_1[0]
+    if sim2>sim1:
+        return profile_2[0]
+    if sim1==sim2:
+        return min(profile_1[0], profile_2[0])
+    else:
+        return None
 
 
 # Mark 8
