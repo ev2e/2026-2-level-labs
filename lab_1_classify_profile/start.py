62b17b1a4ed5e6bacfd15de50d29bd60f094bec0
ev2e <ev2e@MacBook-Air-ev2e.local>
2026-09-24 17:04:22 -0700

    fix style

--- lab_1_classify_profile/start.py
+++ lab_1_classify_profile/start.py
@@ -10,13 +10,16 @@ from main import (
     detect_language_by_top_n,
     get_top_n_words,
     remove_stop_words,
-    tokenize
- )
+    tokenize,
+
+)
+
 
 def main() -> None:
     """
     Launches an implementation.
     """
+
     with open("lab_1_classify_profile/assets/texts/de.txt", "r", encoding="utf-8") as file:
         de_text = file.read()
     with open("lab_1_classify_profile/assets/texts/unknown.txt", "r", encoding="utf-8") as file:
